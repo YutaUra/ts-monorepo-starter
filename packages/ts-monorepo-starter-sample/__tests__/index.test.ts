f6bfa93743660852a6b714e68561fc957da3a66f
YutaUra <yuuta3594@outlook.jp>
2021-05-31 23:04:28 +0900

    chore: jest
    jest integration

--- packages/ts-monorepo-starter-sample/__tests__/index.test.ts
+++ packages/ts-monorepo-starter-sample/__tests__/index.test.ts
@@ -1,7 +1,9 @@
 import { hello } from '../src/index'
 
 describe('@yutaura/ts-monorepo-starter-sample', () => {
-  it('needs tests', () => {
+  it('hello func output "Hello World"', () => {
+    const consoleSpy = jest.spyOn(console, 'log')
     hello()
+    expect(consoleSpy).toHaveBeenCalledWith('Hello World')
   })
 })
